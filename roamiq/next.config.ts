1808ee9fedd235fb6d2b80e41a21b997d0fafec4
Roamiq92 <dany-ben@hotmail.it>
2026-03-17 13:48:22 +0100

    new next config solve issues

--- roamiq/next.config.ts
+++ roamiq/next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // disattivato per build stabile su Vercel
-  const nextConfig = {
   reactCompiler: false,
 };
+
 export default nextConfig;
-};
